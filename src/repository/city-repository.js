b731a119e2d31aa86651cf3cf8dfc5647e9df3dd
Siddhesh202 <siddheshpisal76@gmail.com>
2022-12-25 01:13:34 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -8,19 +8,46 @@ class CityRepository{
             return city;
 
         }catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
-    async deleteCity({name}){
+    async deleteCity(cityId){
         try{
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
-
+            return true;
         }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try{
+            const city = await City.update(data, {
+                where: {
+                    id:cityId
+                }
+            });
+
+            return city;  
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer")
             throw {error};
         }
     }
